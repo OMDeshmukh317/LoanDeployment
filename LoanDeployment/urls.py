dacf7bb17fbff1c531b8cdf4a3c2c1716c9051d2
Om Deshmukh <2010030432cse@gmail.com>
2024-11-22 16:39:36 +0530

    Update urls.py

--- LoanDeployment/urls.py
+++ LoanDeployment/urls.py
@@ -1,6 +1,9 @@
 # from django.contrib import admin
 from django.urls import path,include
 from LoanDeployment import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     # path('',views.index,name="index"),
@@ -9,4 +12,4 @@ urlpatterns = [
     # path('result',views.formInfo,name="result"),
     path('about',views.about,name="about"),
     path('', views.formInfo, name="index"), 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
